a1b28500e862cffcdaa9eefa507c9de049a16060
burdagol5000 <vicenteponteras@gmail.com>
2023-09-09 09:49:00 +0000

    added authentication on views.py

--- members/views.py
+++ members/views.py
@@ -4,4 +4,15 @@ from django.contrib import messages
 
 # Create your views here.
 def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    else 
+
     return render(request,'authentication/login.html',{})

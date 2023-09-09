42e3e13e847d987b5f48532dbe5ddda8b9ec441e
burdagol5000 <vicenteponteras@gmail.com>
2023-09-09 08:10:33 +0000

    created credentials model

--- convos/models.py
+++ convos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Credentials(models.Model):
+    firstname = models.CharField(max_length=20, null=False, blank=False)
+    lastname = models.CharField(max_length=20, null=False, blank=False)
+    email = models.EmailField(max_length=20, null=False, blank=False)
+    username = models.CharField(max_length=10, null=False, blank=False)
+    password = models.CharField(max_length=20, null=False, blank=False)
+    

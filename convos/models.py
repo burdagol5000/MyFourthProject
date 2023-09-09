e339788f4198cb64877eda9fe8a3ae385d7bc169
burdagol5000 <vicenteponteras@gmail.com>
2023-09-09 08:18:17 +0000

    added definition for credentials model

--- convos/models.py
+++ convos/models.py
@@ -7,4 +7,6 @@ class Credentials(models.Model):
     email = models.EmailField(max_length=20, null=False, blank=False)
     username = models.CharField(max_length=10, null=False, blank=False)
     password = models.CharField(max_length=20, null=False, blank=False)
-    
+
+    def __str__(self):
+        return self.name
